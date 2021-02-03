0b94de93871ac5df7ff9e5b2a4f30c61113b8a67
Connielee99 <49182792+Connielee99@users.noreply.github.com>
2021-02-03 13:03:32 +0800

    Update make_dataset.py

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -3,3 +3,22 @@ import os
 from os import listdir
 import pandas as pd
 import datetime
+
+# obtain needed data for processing
+# Filter out data to use volatility on data an hour prior to market open
+# Market Open: NYSE open Monday-Friday 9:30am to 4pm. Eastern time
+def raw_to_data(fp):
+    
+    # read in data
+    data = pd.read_csv(fp)
+    # convert time column time zone
+    data.time = pd.DatetimeIndex(data.time).tz_localize('US/Eastern')
+    # filter out data for use
+    market_open_time = datetime.time(hour=9, minute=30)
+    market_1hr_early = datetime.time(hour=8, minute=30)
+    data = data.loc[data.time.apply(lambda date:(date.time()<=market_open_time)
+                        and (date.time()>=market_1hr_early)
+                        )]
+    data.reset_index(drop=True, inplace=True)
+    
+    return data
