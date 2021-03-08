50bc649cd7b34bb049cb274350b6d6e52cf8af08
Sohyun Lee <sohyunlee33@gmail.com>
2021-03-07 20:45:45 -0800

    update src folder

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -1,18 +1,44 @@
+# imports and dependencies
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+from matplotlib import pyplot as plt
+import datetime as dt
+import numpy as np
+
 '''
-This function builds the Volatility column for modeling
+This function is preprocessing the raw data with extracting first hour after market open. (9:15-10:15)
 '''
-def feature_engineer(data, time_wdw):
+
+def preprocessing(fileName):
+    '''
+    fileName: name of the file for the raw data
+    '''
+    raw = pd.read_csv('../data/raw data/raw_NIFTY100.csv')
+
+    df = raw.copy()
+    df.timestamp = pd.to_datetime(df.timestamp)
+    df['date'] = df.timestamp.apply(lambda x: x.date)
+    df['minute'] = df.timestamp.apply(lambda x: x.time)
+
+    # Getting rid of dates where #data points are insufficient
+    a = pd.DataFrame(df.groupby('date').close.count())
+    drop_dates = list(a[a.close < 300].index)
+    df = df[~df.date.isin(drop_dates)]
     
-    # for time series indexing
-    data.set_index('time', inplace=True)
+    first = df.copy()
+    first['temp'] = first.minute.apply(first_hour)
+    first = first[first.temp==True]
+    first = first.drop(['timestamp', 'open', 'high', 'low', 'volume', 'temp'], axis=1)
     
-    # High-Low within minute bar
-    data['diff'] = data.high-data.low
-    # Volitility Analysis
-    vol = data['diff'].rolling(time_wdw).mean()
-    data = data.assign(Volatility=vol)
-    # for modelling purpose
-    data.dropna(axis=0, inplace=True)
-    data.reset_index(inplace = True)
+    first.to_csv('../data/first_combined.csv',index=False)
     
-    return data
+
+def first_hour(time):
+    if time.hour == 9:
+        if time.minute >=15:
+            return True
+    elif time.hour == 10:
+        if time.minute < 15:
+            return True
+    else:
+        return False
